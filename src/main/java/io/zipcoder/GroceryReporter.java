712aa8039800b22f1cb21177169e08462539a2e8
AndrewAscone <andrew.ascone@gmail.com>
2022-12-12 17:04:54 -0500

    fixed some bugs

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,9 +1,15 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+import io.zipcoder.utils.ItemParseException;
+
+import java.util.List;
 
 public class GroceryReporter {
     private final String originalFileText;
+
+    ItemParser parser = new ItemParser();
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
@@ -11,6 +17,14 @@ public class GroceryReporter {
 
     @Override
     public String toString() {
+        //System.out.println(parser.parseItemList(originalFileText));
+          List<Item> list = parser.parseItemList(originalFileText);
+          for (Item i :list) {
+              System.out.println(i);
+          }
+
+          //        System.out.println(parser.parseItemList(originalFileText).size());
+
         return null;
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -6,20 +6,22 @@ import io.zipcoder.utils.ItemParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse){
         List<Item> parsedList = new ArrayList<>();
         int exceptionCount = 0;
 
-        String[] parsed = valueToParse.toLowerCase().split("#");
-        System.out.println(Arrays.toString(parsed));
+        String[] parsed = valueToParse.toLowerCase().split("##");
+        //System.out.println(Arrays.toString(parsed));
 
         for(String s : parsed){
             try{
                 parsedList.add(parseSingleItem(s));
             } catch (ItemParseException e) {
                 exceptionCount++;
+                System.out.println("Exception handled. Exception count is now: " + exceptionCount);
             }
         }
         System.out.println(parsedList);
@@ -28,13 +30,16 @@ public class ItemParser {
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
         String[] parsed = singleItem.toLowerCase().split("[:;^%*!@#]");
-        System.out.println(Arrays.toString(parsed));
-        if(parsed.length != 8){
-            System.out.println("Parsed item has improper length!");
+        //System.out.println(Arrays.toString(parsed));
+        if(parsed.length != 8 || Objects.equals(parsed[1], "")){
+            //System.out.println("Parsed item has improper length!");
            throw new ItemParseException();
         }
         else {
-            double priceValue = Double.parseDouble(parsed[3]);
+            double priceValue;
+            try{
+                priceValue = Double.parseDouble(parsed[3]);
+            }catch(NumberFormatException e){throw new ItemParseException();}
             return new Item(parsed[1], priceValue, parsed[5], parsed[7]);
         }
     }
